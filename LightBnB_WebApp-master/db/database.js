ae9866424fd3a4ebdac8d53a05b078f5e0b397d3
Abdi-Raheem <raheem129@hotmail.com>
2023-06-29 12:31:34 -0400

    refactored reservations function

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -85,7 +85,6 @@ const addUser = function (user) {
     .then(res => res.rows[0])
     .catch(err => console.error('Error executing query', err));
 };
-
 /// Reservations
 
 /**
@@ -94,8 +93,23 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const query = `
+    SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+    FROM reservations
+    JOIN properties ON reservations.property_id = properties.id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1
+    GROUP BY reservations.id, properties.id
+    ORDER BY reservations.start_date
+    LIMIT $2;
+  `;
+
+  const values = [guest_id, limit];
+
+  return pool.query(query, values)
+    .then(res => res.rows);
 };
+
 
 /// Properties
 
